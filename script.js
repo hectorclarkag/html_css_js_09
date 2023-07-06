a2c03314c7d10359a5df0961eeadea2a4f21243a
Heitor <49591729+hectorclakrag@users.noreply.github.com>
2023-07-06 11:25:25 +0100

    adicionando anotacoes

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
+// pega-se os elementos do html e guarda-se em variaveis e constantes
 const notesContainer = document.querySelector(".notes_container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input_box");
 
+
+// pega o que tem no local storage e mostra na tela
 function showNotes() {
     notesContainer.innerHTML = localStorage.getItem("notes");
 }
 showNotes();
 
+// da o update no local storage
 function updateStorage() {
     localStorage.setItem("notes", notesContainer.innerHTML);
 }
 
+// criar-se o bloco de nota
 createBtn.addEventListener("click", ()=>{
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
@@ -20,6 +25,7 @@ createBtn.addEventListener("click", ()=>{
     notesContainer.appendChild(inputBox).appendChild(img);
 })
 
+// permite a remocao de cada note e da o update no local storage
 notesContainer.addEventListener("click", function(e){
     if(e.target.tagName === "IMG"){
         e.target.parentElement.remove();
@@ -35,6 +41,7 @@ notesContainer.addEventListener("click", function(e){
     }
 })
 
+// Previne a quebra do codigo
 document.addEventListener("keydown", event => {
     if(event.key === "Enter"){
         document.execCommand("insertLineBreak");
